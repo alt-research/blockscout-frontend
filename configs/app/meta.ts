3d9b5c2c1541155f810a119596befef508595c5d
tom <tom@ohhhh.me>
2025-01-22 08:22:59 +0100

    Merge branch 'main' of github.com:blockscout/frontend into tom2drum/rollbar-fix

--- configs/app/meta.ts
+++ configs/app/meta.ts
@@ -1,27 +1,32 @@
-import app from './app';
-import { getEnvValue, getExternalAssetFilePath } from './utils';
+import app from "./app";
+import { getEnvValue, getExternalAssetFilePath } from "./utils";
 
-const defaultImageUrl = '/static/og_placeholder.png';
-const path = getExternalAssetFilePath('NEXT_PUBLIC_OG_IMAGE_URL');
+const defaultImageUrl = "/static/og_placeholder.png";
+const path = getExternalAssetFilePath("NEXT_PUBLIC_OG_IMAGE_URL");
 let ogImageUrl: URL;
 
 // Handle Next.js to collect page data
 try {
   ogImageUrl = new URL(path || defaultImageUrl, app.baseUrl);
 } catch (e) {
-  ogImageUrl = new URL('', 'https://placehodler');
+  ogImageUrl = new URL("", "https://placehodler");
 }
 
 const meta = Object.freeze({
-  promoteBlockscoutInTitle: getEnvValue('NEXT_PUBLIC_PROMOTE_BLOCKSCOUT_IN_TITLE') === 'false' ? false : true,
+  promoteBlockscoutInTitle:
+    getEnvValue("NEXT_PUBLIC_PROMOTE_BLOCKSCOUT_IN_TITLE") === "false"
+      ? false
+      : true,
   og: {
-    title: getEnvValue('NEXT_PUBLIC_OG_TITLE') || '',
-    description: getEnvValue('NEXT_PUBLIC_OG_DESCRIPTION') || '',
+    title: getEnvValue("NEXT_PUBLIC_OG_TITLE") || "",
+    description: getEnvValue("NEXT_PUBLIC_OG_DESCRIPTION") || "",
     imageUrl: ogImageUrl,
-    enhancedDataEnabled: getEnvValue('NEXT_PUBLIC_OG_ENHANCED_DATA_ENABLED') === 'true',
+    enhancedDataEnabled:
+      getEnvValue("NEXT_PUBLIC_OG_ENHANCED_DATA_ENABLED") === "true",
   },
   seo: {
-    enhancedDataEnabled: getEnvValue('NEXT_PUBLIC_SEO_ENHANCED_DATA_ENABLED') === 'true',
+    enhancedDataEnabled:
+      getEnvValue("NEXT_PUBLIC_SEO_ENHANCED_DATA_ENABLED") === "true",
   },
 });
 
