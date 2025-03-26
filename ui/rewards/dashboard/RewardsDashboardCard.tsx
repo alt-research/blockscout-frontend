660f5e3166955956c8622b93fb3aa3dbb7ca447b
carlomigueldy <45052332+carlomigueldy@users.noreply.github.com>
2025-03-26 15:15:15 +0800

    Revert "Merits dashboard minor changes (#2637)"
    This reverts commit b34efd2d4b8d980f2c812ed22666f368f1ed4d28.

--- ui/rewards/dashboard/RewardsDashboardCard.tsx
+++ ui/rewards/dashboard/RewardsDashboardCard.tsx
@@ -1,12 +1,9 @@
 import { Flex, Text, useColorModeValue, Tag } from '@chakra-ui/react';
 import React from 'react';
 
-import HintPopover from 'ui/shared/HintPopover';
-
 type Props = {
-  title: string;
+  title?: string;
   description: string | React.ReactNode;
-  hint?: string | React.ReactNode;
   availableSoon?: boolean;
   blurFilter?: boolean;
   contentAfter?: React.ReactNode;
@@ -16,7 +13,7 @@ type Props = {
 };
 
 const RewardsDashboardCard = ({
-  title, description, hint, availableSoon, contentAfter,
+  title, description, availableSoon, contentAfter,
   direction = 'column', children, blurFilter,
 }: Props) => {
   return (
@@ -37,19 +34,12 @@ const RewardsDashboardCard = ({
         p={{ base: 1.5, md: 3 }}
         w={{ base: 'full', md: direction === 'row' ? '340px' : 'full' }}
       >
-        <Flex alignItems="center" gap={ 2 }>
-          <Text fontSize={{ base: 'md', md: 'lg' }} fontWeight="500">{ title }</Text>
-          { hint && (
-            <HintPopover
-              label={ hint }
-              popoverContentProps={{
-                maxW: { base: 'calc(100vw - 8px)', lg: '210px' },
-              }}
-              popoverBodyProps={{ textAlign: 'center' }}
-            />
-          ) }
-          { availableSoon && <Tag colorScheme="blue">Available soon</Tag> }
-        </Flex>
+        { title && (
+          <Flex alignItems="center" gap={ 2 }>
+            <Text fontSize={{ base: 'md', md: 'lg' }} fontWeight="500">{ title }</Text>
+            { availableSoon && <Tag colorScheme="blue">Available soon</Tag> }
+          </Flex>
+        ) }
         <Text as="div" fontSize="sm">
           { description }
         </Text>
